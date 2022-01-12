d446a8d3eaaa126b4d23cea8924aadc4b424d539
Steve Jaehyeok Kim <stevejh.kim0322@gmail.com>
2022-01-12 17:20:44 +0800

    Cover for Bio Page

--- src/pages/artist-bio.tsx
+++ src/pages/artist-bio.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import BioCover from "../components/bio-cover";
 
 import Template from "./template";
 
 function ArtistBio() {
-  return <Template pageName="artist-bio">ArtistBio</Template>;
+  return (
+    <Template pageName="artist-bio">
+      <BioCover />
+    </Template>
+  );
 }
 
 export default ArtistBio;
