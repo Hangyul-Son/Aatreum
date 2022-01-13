23a08290b72e123d64a97a73b966a1dfce3b3846
Steve Jaehyeok Kim <stevejh.kim0322@gmail.com>
2022-01-13 19:00:39 +0800

    Changed image ratio, and the arrangement of the cards

--- src/components/bio/bio-portfolio.tsx
+++ src/components/bio/bio-portfolio.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Col, Row, Image, Card } from "antd";
 import "./bio-portfolio.scss";
 
-const carSource = ["car1.png", "car2.png", "car3.png", "car4.png"];
+const carSource = ["car1.jpg", "car2.jpg", "car3.jpg", "car4.jpg"];
 
 function BioPortfolio() {
   return (
     <div className="pf-container">
       <Row className="pf-row">
         {carSource.map((imageSource, index) => (
-          <Col span={12}>
+          <Col xs={24} sm={24} md={24} lg={12} xl={12} className="pf-col">
             <Card
               hoverable
-              style={{ height: "500px" }}
+              style={{ height: "100%" }}
               cover={
                 <Image
-                  style={{ height: "300px" }}
+                  style={{ height: "70%" }}
                   preview={false}
                   src={window.location.origin + "/assets/images/" + imageSource}
                 />
