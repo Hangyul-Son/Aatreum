c3ea37d7082226d2bcda051ad6e6869d0d2bd26c
Han <hangyulson1004@gmail.com>
2022-01-13 23:00:48 +0800

    Create funding notice bar

--- src/components/funding-main.tsx
+++ src/components/funding-main.tsx
@@ -5,20 +5,29 @@ import "./funding-main.scss";
 import FundingCover from "./funding-cover";
 import FundingTitle from "./funding-title";
 import FundingDescription from "./funding-description";
+import FundingNotice from "./funding-notice";
+import FundingDescriptionSpecific from "./funding-description-specific";
 
 function FundingMain() {
   return (
-    <Row className="funding-main">
-      <Col span={24}>
-        <FundingTitle />
-      </Col>
-      <Col span={15}>
-        <FundingCover />
-      </Col>
-      <Col span={9}>
-        <FundingDescription />
-      </Col>
-    </Row>
+    <div className="funding-main-container">
+      <Row className="funding-main-upperhalf">
+        <Col span={24}>
+          <FundingTitle />
+        </Col>
+        <Col span={15}>
+          <FundingCover />
+        </Col>
+        <Col span={9}>
+          <FundingDescription />
+        </Col>
+      </Row>
+      <FundingNotice />
+			<Row className="funding-main-lowerhalf">
+				<Col span={14}>
+					<FundingDescriptionSpecific />
+			</Row>
+    </div>
   );
 }
 
