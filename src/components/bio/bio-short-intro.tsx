af23a6972cb74d26aa271f9a41f80dbdfdd041e7
ntxq <bkwak@ust.hk>
2022-01-13 19:26:12 +0800

    Update bio-cover responsive styles

--- src/components/bio/bio-short-intro.tsx
+++ src/components/bio/bio-short-intro.tsx
@@ -24,25 +24,6 @@ function BioShortIntro() {
         </Space>
       </Col>
     </Row>
-
-    // <div className="short-intro-container">
-    //   <div className="container-group">
-    //     <div className="col-1">
-    //       <Typography.Title level={2} className="col-1-text">
-    //         I'm in <em>love</em> with cars
-    //       </Typography.Title>
-    //     </div>
-
-    //     <div className="col-2">
-    //       <Text strong>ABOUT GUNWOO BAEK</Text>
-    //       <br />
-    // <Text>
-    //   He is an awesome amateur photographer He is an awesome amateur
-    //   photographer He is an awesome amateur photographer
-    // </Text>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
