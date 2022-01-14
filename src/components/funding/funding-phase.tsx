2d1cf8bcba1d663569aba31935757dbd6e010e73
ntxq <bkwak@ust.hk>
2022-01-15 01:01:55 +0800

    Change funding steps styles

--- src/components/funding/funding-phase.tsx
+++ src/components/funding/funding-phase.tsx
@@ -7,10 +7,10 @@ const { Step } = Steps;
 function FundingPhase() {
   return (
     <div className="funding-phase-container">
-      <Steps percent={40} current={1}>
-        <Step title="Phase One" />
-        <Step title="Phase Two" />
-        <Step title="Phase Three" />
+      <Steps percent={40} current={1} responsive={false}>
+        <Step title="Funding Published" />
+        <Step title="Gathering Supporters" />
+        <Step title="NFT Minted" />
       </Steps>
     </div>
   );
