b4c8303792e2e743a5cbdbd4fc2a7b93af025e67
ntxq <bkwak@ust.hk>
2022-01-14 13:52:35 +0800

    Change bio sort-by dropdown styles

--- src/components/bio/portfolio-sort-by.tsx
+++ src/components/bio/portfolio-sort-by.tsx
@@ -1,47 +1,36 @@
 import React from "react";
 import { Col, Dropdown, Menu, Space, Typography } from "antd";
+import "./portfolio-sort-by.scss";
 
 function PortfolioSortBy() {
   return (
-    <Col xs={24} sm={12} className="pf-sort">
+    <Col xs={24} sm={12} className="bio-sort">
       <Dropdown
+        overlayClassName="bio-sort-overlay"
         overlay={
           <Menu>
             <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.antgroup.com"
-              >
-                1st menu item
-              </a>
+              <Typography.Text strong>Porsche</Typography.Text>
             </Menu.Item>
             <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.aliyun.com"
-              >
-                2nd menu item (disabled)
-              </a>
+              <Typography.Text strong>Tesla</Typography.Text>
             </Menu.Item>
-            <Menu.Item disabled>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.luohanacademy.com"
-              >
-                3rd menu item (disabled)
-              </a>
+            <Menu.Item>
+              <Typography.Text strong>Mazda</Typography.Text>
             </Menu.Item>
-            <Menu.Item danger>a danger item</Menu.Item>
+            <Menu.Item>
+              <Typography.Text strong>Jaguar</Typography.Text>
+            </Menu.Item>
+            <Menu.Item>
+              <Typography.Text strong>Mitsubishi</Typography.Text>
+            </Menu.Item>
           </Menu>
         }
         className="bio-sort-dropdown"
       >
         <Space size={90}>
-          <Typography.Text>Sort By</Typography.Text>
-          <Typography.Text>+</Typography.Text>
+          <Typography.Text strong>SORT BY</Typography.Text>
+          <Typography.Text strong>+</Typography.Text>
         </Space>
       </Dropdown>
     </Col>
