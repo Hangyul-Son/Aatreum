6e4aa5217f042c26a73a4e35a81b1376a21df6c7
ntxq <bkwak@ust.hk>
2022-01-14 13:22:02 +0800

    Fix bio image cover overlay

--- src/components/bio/bio-cover.tsx
+++ src/components/bio/bio-cover.tsx
@@ -21,13 +21,14 @@ function BioCover() {
         opacity: 1 - scrollTop / 820,
       }}
     >
-      <Image
-        src={`${window.location.origin}/assets/images/bgw.png`}
-        preview={false}
-        alt="Gunwoo Baek"
-        className="bio-cover"
-      />
-      <div className="cover-mask" />
+      <div className="cover-image-overlay">
+        <Image
+          src={`${window.location.origin}/assets/images/bgw.png`}
+          preview={false}
+          alt="Gunwoo Baek"
+          className="bio-cover"
+        />
+      </div>
 
       <div className="bio-cover-text">
         <Typography.Title level={1} className="bio-cover-title">
